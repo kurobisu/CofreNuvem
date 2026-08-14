c70f11ef808f95a17a179544a93723a609556a04
Clóvis <clovis.menuvem@gmail.com>
2026-08-14 13:48:35 -0300

    security: remove find_db.dart from git tracking

--- find_db.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-
-void main() async {
-  sqfliteFfiInit();
-  var dbFactory = databaseFactoryFfi;
-  String dbPath = await dbFactory.getDatabasesPath();
-  print('DB PATH: \$dbPath');
-}
